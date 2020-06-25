bd4201d1e755bbdf36477dc1011be8aa8a399320
VladislavSakhno13 <sahno.99@inbox.ru>
2020-06-25 16:55:25 +0300

    head menu

--- front/app.jsx
+++ front/app.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import axios from 'axios';
 import Admin_page from './adminpage.jsx';
 import Navigation from './navigation.jsx';
-ReactDOM.render(<Navigation/>,document.getElementById('navigation'));
-ReactDOM.render(<Admin_page/>,document.getElementById('admin'));
+import Start_page from './start_page.jsx';
+ReactDOM.render(<Start_page/>,document.getElementById('page_site'))
+/*ReactDOM.render(<Navigation/>,document.getElementById('navigation'));
+ReactDOM.render(<Admin_page/>,document.getElementById('admin'));*/
 
