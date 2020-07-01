f458c332a9c1bf00fa32fcae5eed0a172ab8ce90
VladislavSakhno13 <sahno.99@inbox.ru>
2020-07-01 23:12:10 +0300

    fix sent array

--- backend/i.php
+++ backend/i.php
@@ -3,7 +3,7 @@
         session_start();
         $data_with_user = array('username'=>$_SESSION['username'],
         'status'=>$_SESSION['status'],'id'=>$_SESSION['id']);
-        exit($data_with_user['id']);
+        exit(json_encode($data_with_user));
         //echo $_SESSION['username'];
         //echo $_SESSION['status'];
         //session_destroy();
