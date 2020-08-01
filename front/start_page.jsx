9b1b22541dae9d7fe5915d165633f5bcb1c7545e
VladislavSakhno13 <sahno.99@inbox.ru>
2020-08-01 14:18:25 +0300

    new state

--- front/start_page.jsx
+++ front/start_page.jsx
@@ -14,6 +14,16 @@ import Card_products from './card_products.jsx';
 import Main_component from './main_component.jsx';
 
 export default class Start_page extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            state: "A"
+        }
+        this.changeStateProducts=this.changeStateProducts.bind(this);
+    }
+    changeStateProducts(){
+       this.setState({state:"B"})
+    }
     get_prducts(){
         //ReactDOM.unmountComponentAtNode(document.getElementById('main_page'));
         if(document.querySelectorAll('#list-product>div').length === 0){
@@ -54,39 +64,54 @@ export default class Start_page extends React.Component{
         ReactDOM.render(<Sign_in/>,document.getElementById('sign_up'))
     }
     render(){
-        
+        if(this.state.state === "A"){
         return(
             <div>
-                <Navbar bg="dark" variant="dark">
-                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                        <Nav className="mr-auto">
-                        <Nav.Link onClick={this.comeback}>Главная страница</Nav.Link>
-                        <Nav.Link onClick={this.get_prducts}>Список товаров</Nav.Link>
+                <div>
+                    <Navbar bg="dark" variant="dark">
+                            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                            <Nav className="mr-auto">
+                            <Nav.Link >Главная страница</Nav.Link>
+                            <Nav.Link onClick={this.changeStateProducts}>Список товаров</Nav.Link>
 
-                       
+                        
 
-                        <Nav.Link id="open_admin_page" onClick={this.openAdminpage}>Admin page</Nav.Link>
-                        </Nav>
-                        <Form inline>
-                        <ButtonGroup aria-label="Basic example">
-                            <Button variant="secondary" id="sign-in" onClick={this.openSign_in}>Авторизация</Button>
-                            <Button variant="secondary" id="sign-up" onClick={this.openSign_up}>Регистрация</Button>
-                            
+                            <Nav.Link id="open_admin_page" onClick={this.openAdminpage}>Admin page</Nav.Link>
+                            </Nav>
+                            <Form inline>
+                            <ButtonGroup aria-label="Basic example">
+                                <Button variant="secondary" id="sign-in" onClick={this.openSign_in}>Авторизация</Button>
+                                <Button variant="secondary" id="sign-up" onClick={this.openSign_up}>Регистрация</Button>
+                                
 
-                            <NavDropdown title="Пользователь" id="basic-nav-dropdown" id="menu-for-user">
-                                <NavDropdown.Item onClick={this.open_basket}>Моя корзина</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
+                                <NavDropdown title="Пользователь" id="basic-nav-dropdown" id="menu-for-user">
+                                    <NavDropdown.Item onClick={this.open_basket}>Моя корзина</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                </NavDropdown>
 
-                            <Button variant="secondary" id="out">Выход</Button>
-                        </ButtonGroup>
-                        </Form>
-                </Navbar>
-  <br />
+                                <Button variant="secondary" id="out">Выход</Button>
+                            </ButtonGroup>
+                            </Form>
+                    </Navbar>
+                </div>
+                <div id="main_inform">
+                    hh
+                </div>
+ 
             </div>
         )
+        }
+
+        if(this.state.state === "B"){
+            return(
+                <div>
+                    <div>123</div>
+                    <div>123</div>
+                </div>
+            )
+        }
     }
 }
